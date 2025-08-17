ee51b1626c19797c540cb630da553987aaa1ce5e
emblog <50002876+emblog@users.noreply.github.com>
2025-08-17 17:20:37 +0900

    로컬 테스트 할 때 문제 수정

--- server_on.py
+++ server_on.py
@@ -17,7 +17,15 @@ if len(sys.argv) >= 2:
 else:
 	PORT = DEFAULT_PORT
 
-Handler = http.server.SimpleHTTPRequestHandler
+# CORS 헤더를 추가하는 핸들러
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(CORSRequestHandler, self).end_headers()
+
+Handler = CORSRequestHandler
 
 print(f"로컬 서버 시작 중... (http://localhost:{PORT})")
 
